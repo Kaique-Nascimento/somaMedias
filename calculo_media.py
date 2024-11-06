7c30806c7b6dc14a72f6f974268f86c8142d0d90
Kaique Nascimento <kaiquepalmanascimento@hotmail.com>
2024-11-05 22:02:12 -0300

    Add files via upload

---
+++ calculo_media.py
@@ -0,0 +1,49 @@
+#inicialização de variáveis
+numerosDigitados = []
+contador = 0
+soma = 0
+media = 0
+
+#laço que executará o programa até que o número 0 seja digitado
+while True:
+
+    #ve se o codigo nao dara erro
+    try:
+       
+        numero = float(input(f"Digite um número: (0 para fazer o calculo da media)  "))
+
+    #se valor digitado nao for um numero
+    except ValueError:
+        print("Digite um número válido!")
+
+        #volta pro começo
+        continue
+    
+    #autoexplicativo
+    if numero == 0:
+        print(f"Os números digitados: \n-=-=-=-=-=")
+
+        #percorre os numeros digitados
+        for numeros in numerosDigitados:
+
+            #mostra numeros digitados
+            print(numeros)
+
+            #soma os numeros digitados
+            soma+=numeros
+
+            #faz a media entre eles
+            media = soma/contador
+
+        print("-=-=-=-=-=")
+
+        #:.2f mostra 2 números depois da vírgula
+        print(f"A soma deles é de: {soma:.2f}")
+        print(f"A média entre eles é de: {media:.2f}")
+        print(f"A quantidade de números digitados foi de: {contador}")
+        break
+
+    #continua o código se o numero for diferente de 0
+    contador+=1
+    numerosDigitados.append(numero)
+    
